48d9740b69a503b94ee3cc3d04a10614ea0300fe
Anton <varan@teasernet.com>
2017-06-14 07:43:23 +0300

    fix json

--- src/main/kotlin/hello/WebSocketsHandler.kt
+++ src/main/kotlin/hello/WebSocketsHandler.kt
@@ -1,19 +1,22 @@
 package hello
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 import java.util.concurrent.ConcurrentHashMap
-import java.util.logging.Logger
+import org.json.JSONObject
+
 
 @Service
 class WebSocketsHandler() : TextWebSocketHandler() {
 
-    @Autowired
-    lateinit var log: Logger;
-    private val sessions: ConcurrentHashMap<String, WebSocketSession> = ConcurrentHashMap<String, WebSocketSession>();
+    private val logger:Logger = LoggerFactory.getLogger(this.javaClass)
+    private val sessions = ConcurrentHashMap<String, WebSocketSession>();
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         sessions.put(session.id, session);
@@ -21,6 +24,8 @@ class WebSocketsHandler() : TextWebSocketHandler() {
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         //super.handleTextMessage(session, message)
-        log.info(message.payload);
+        logger.debug(message.payload);
+        val answer = JSONObject(message.payload);
+        logger.debug("{}", answer.getDouble("newBid"));
     }
 }
